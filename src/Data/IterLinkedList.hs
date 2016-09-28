781ab158e8635793077885d085437bc6100b444f
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2016-09-28 17:13:49 +0800

    first version with test

--- src/Data/IterLinkedList.hs
+++ src/Data/IterLinkedList.hs
@@ -1,3 +1,307 @@
+{-# LANGUAGE TypeFamilies, NamedFieldPuns, RecordWildCards, FlexibleInstances #-}
+
 module Data.IterLinkedList
-  (
+  ( IterLinkedList (..)
+  , LinkedList
+  , firstIter
+  , lastIter
   ) where
+
+import qualified Data.IntMap.Strict as IM
+import qualified Data.Map.Strict as M
+
+data LinkedList iter value = LinkedList
+  { newKey :: iter -- ^ pre-allocated iterator value for the next inserted element
+  , firstKey :: iter -- ^ iterator to the first element (equals to `newKey` when `null container`)
+  , lastKey :: iter -- ^ iterator to the last element (equals to `newKey` when `null container`)
+  , container :: LinkedListContainer iter value
+  }
+
+instance Show value => Show (LinkedList Int value) where
+  show ls = "fromList " ++ show (toList ls)
+
+instance Show value => Show (LinkedList Integer value) where
+  show ls = "fromList " ++ show (toList ls)
+
+type family LinkedListContainer iter value
+type instance LinkedListContainer Int value = IM.IntMap (Int, value, Int)
+type instance LinkedListContainer Integer value = M.Map Integer (Integer, value, Integer)
+
+class Enum iter => IterLinkedList iter where
+  null :: LinkedList iter value -> Bool
+
+  -- | Get the element value
+  get :: iter -> LinkedList iter value -> Maybe value
+
+  -- | Get the element value. Get undefined if not found
+  get' :: iter -> LinkedList iter value -> value
+
+  -- | Get the next iterator.
+  --   If the specified iterator is the last one, or isn't in the list,
+  --   return the original one.
+  next :: LinkedList iter value -> iter -> iter
+
+  -- | Get the previous iterator.
+  --   If the specified iterator is the first one, or isn't in the list,
+  --   return the original one.
+  prev :: LinkedList iter value -> iter -> iter
+
+  -- | Get an empty list
+  empty :: LinkedList iter value
+
+  -- | Get a list with exactly one element
+  singleton :: value -> LinkedList iter value
+
+  -- | Insert a new element before the specified iterator.
+  --   If the list is empty, just insert the new element as the only element.
+  --   If the specified iterator can't be found, prepend the new element to the whole list.
+  insertBefore :: iter -> value -> LinkedList iter value -> LinkedList iter value
+
+  -- | Insert a new element after the specified iterator.
+  --   If the list is empty, just insert the new element as the only element.
+  --   If the specified iterator can't be found, append the new element to the whole list.
+  insertAfter :: iter -> value -> LinkedList iter value -> LinkedList iter value
+
+  -- | Delete the specified element from the list.
+  --   If there's no such element in the list, return the original list.
+  delete :: iter -> LinkedList iter value -> LinkedList iter value
+
+  -- | Get a LinkedList from a list
+  fromList :: [value] -> LinkedList iter value
+
+  -- | Get a list from a LinkedList
+  toList :: LinkedList iter value -> [value]
+
+-- | Get the first iterator.
+--   If the list is empty, you'll still get an unusable one.
+--   You can't get the value from the unusable iterator.
+firstIter :: LinkedList iter value -> iter
+firstIter LinkedList{firstKey} = firstKey
+
+-- | Get the last iterator.
+--   If the list is empty, you'll still get an unusable one.
+--   You can't get the value from the unusable iterator.
+lastIter :: LinkedList iter value -> iter
+lastIter LinkedList{lastKey} = lastKey
+
+instance IterLinkedList Int where
+
+  null (LinkedList {container = cntr}) = IM.null cntr
+
+  get iter (LinkedList {..}) =
+    fmap (\(prev, value, next) -> value) (IM.lookup iter container)
+
+  get' iter (LinkedList {..}) = case IM.lookup iter container of
+    Just (prev, value, next) -> value
+    _ -> undefined
+
+  next (LinkedList {..}) iter = case IM.lookup iter container of
+    Just (prev, value, next) -> next
+    _ -> iter
+
+  prev (LinkedList {..}) iter = case IM.lookup iter container of
+    Just (prev, value, next) -> prev
+    _ -> iter
+
+  empty = LinkedList
+    { newKey = minBound
+    , firstKey = minBound
+    , lastKey = minBound
+    , container = IM.empty
+    }
+
+  singleton value = LinkedList
+    { newKey = minBound + 1
+    , firstKey = minBound
+    , lastKey = minBound
+    , container = IM.singleton minBound (minBound, value, minBound)
+    }
+
+  insertBefore iter value (LinkedList {..}) = LinkedList
+    { newKey = newKey + 1
+    , firstKey = if isPrepend then newKey else firstKey
+    , lastKey = lastKey
+    , container
+      = IM.insert newKey (prevKey, value, nextKey)
+      $ IM.adjust
+        (\(prevPrevKey, prevValue, prevNextKey) -> (prevPrevKey, prevValue, newKey))
+        prevKey
+      $ IM.adjust
+        (\(nextPrevKey, nextValue, nextNextKey) -> (newKey, nextValue, nextNextKey))
+        nextKey
+      $ container
+    }
+    where
+      (isPrepend, prevKey, nextKey) = case IM.lookup iter container of
+        Nothing -> (True, newKey, firstKey)
+        Just (iterPrevKey, iterValue, iterNextKey)
+          | iterPrevKey == iter -> (True, newKey, iter)
+          | otherwise -> (False, iterPrevKey, iter)
+
+  insertAfter iter value (LinkedList {..}) = LinkedList
+    { newKey = newKey + 1
+    , firstKey = firstKey
+    , lastKey = if isAppend then newKey else lastKey
+    , container
+      = IM.insert newKey (prevKey, value, nextKey)
+      $ IM.adjust
+        (\(prevPrevKey, prevValue, prevNextKey) -> (prevPrevKey, prevValue, newKey))
+        prevKey
+      $ IM.adjust
+        (\(nextPrevKey, nextValue, nextNextKey) -> (newKey, nextValue, nextNextKey))
+        nextKey
+      $ container
+    }
+    where
+      (isAppend, prevKey, nextKey) = case IM.lookup iter container of
+        Nothing -> (True, lastKey, newKey)
+        Just (iterPrevKey, iterValue, iterNextKey)
+          | iterNextKey == iter -> (True, iter, newKey)
+          | otherwise -> (False, iter, iterNextKey)
+
+  delete iter list@(LinkedList {..}) = case IM.lookup iter container of
+    Nothing -> list
+    Just (iterPrevKey, iterValue, iterNextKey) -> LinkedList
+      { newKey = newKey
+      , firstKey = if firstKey == iter then iterNextKey else firstKey
+      , lastKey = if lastKey == iter then iterPrevKey else lastKey
+      , container
+        = IM.adjust (\(prevPrevKey, prevValue, prevNextKey) ->
+            (prevPrevKey, prevValue, if iterNextKey == iter then iterPrevKey else iterNextKey)
+          ) iterPrevKey
+        $ IM.adjust (\(nextPrevKey, nextValue, nextNextKey) ->
+            (if iterPrevKey == iter then iterNextKey else iterPrevKey, nextValue, nextNextKey)
+          ) iterNextKey
+        $ IM.delete iter container
+      }
+
+  fromList [] = empty
+  fromList as = LinkedList
+    { newKey = minBound + len
+    , firstKey = minBound
+    , lastKey = lastKey
+    , container = IM.fromList $ zip [minBound..] $ zip3 (minBound : [minBound..]) as ([minBound + 1 .. lastKey] ++ [lastKey])
+    }
+    where
+      lastKey = minBound + len - 1
+      len = length as
+
+  toList (LinkedList {..})
+    | IM.null container = []
+    | otherwise = go firstKey where
+      go key = case IM.lookup key container of
+        Just (_, value, nextKey) ->
+          value : (if nextKey == key then [] else go nextKey)
+        _ ->
+          []
+
+instance IterLinkedList Integer where
+
+  null (LinkedList {container = cntr}) = M.null cntr
+
+  get iter (LinkedList {..}) =
+    fmap (\(prev, value, next) -> value) (M.lookup iter container)
+
+  get' iter (LinkedList {..}) = case M.lookup iter container of
+    Just (prev, value, next) -> value
+    _ -> undefined
+
+  next (LinkedList {..}) iter = case M.lookup iter container of
+    Just (prev, value, next) -> next
+    _ -> iter
+
+  prev (LinkedList {..}) iter = case M.lookup iter container of
+    Just (prev, value, next) -> prev
+    _ -> iter
+
+  empty = LinkedList
+    { newKey = 0
+    , firstKey = 0
+    , lastKey = 0
+    , container = M.empty
+    }
+
+  singleton value = LinkedList
+    { newKey = 1
+    , firstKey = 0
+    , lastKey = 0
+    , container = M.singleton 0 (0, value, 0)
+    }
+
+  insertBefore iter value (LinkedList {..}) = LinkedList
+    { newKey = newKey + 1
+    , firstKey = if isPrepend then newKey else firstKey
+    , lastKey = lastKey
+    , container
+      = M.insert newKey (prevKey, value, nextKey)
+      $ M.adjust
+        (\(prevPrevKey, prevValue, prevNextKey) -> (prevPrevKey, prevValue, newKey))
+        prevKey
+      $ M.adjust
+        (\(nextPrevKey, nextValue, nextNextKey) -> (newKey, nextValue, nextNextKey))
+        nextKey
+      $ container
+    }
+    where
+      (isPrepend, prevKey, nextKey) = case M.lookup iter container of
+        Nothing -> (True, newKey, firstKey)
+        Just (iterPrevKey, iterValue, iterNextKey)
+          | iterPrevKey == iter -> (True, newKey, iter)
+          | otherwise -> (False, iterPrevKey, iter)
+
+  insertAfter iter value (LinkedList {..}) = LinkedList
+    { newKey = newKey + 1
+    , firstKey = firstKey
+    , lastKey = if isAppend then newKey else lastKey
+    , container
+      = M.insert newKey (prevKey, value, nextKey)
+      $ M.adjust
+        (\(prevPrevKey, prevValue, prevNextKey) -> (prevPrevKey, prevValue, newKey))
+        prevKey
+      $ M.adjust
+        (\(nextPrevKey, nextValue, nextNextKey) -> (newKey, nextValue, nextNextKey))
+        nextKey
+      $ container
+    }
+    where
+      (isAppend, prevKey, nextKey) = case M.lookup iter container of
+        Nothing -> (True, lastKey, newKey)
+        Just (iterPrevKey, iterValue, iterNextKey)
+          | iterNextKey == iter -> (True, iter, newKey)
+          | otherwise -> (False, iter, iterNextKey)
+
+  delete iter list@(LinkedList {..}) = case M.lookup iter container of
+    Nothing -> list
+    Just (iterPrevKey, iterValue, iterNextKey) -> LinkedList
+      { newKey = newKey
+      , firstKey = if firstKey == iter then iterNextKey else firstKey
+      , lastKey = if lastKey == iter then iterPrevKey else lastKey
+      , container
+        = M.adjust (\(prevPrevKey, prevValue, prevNextKey) ->
+            (prevPrevKey, prevValue, if iterNextKey == iter then iterPrevKey else iterNextKey)
+          ) iterPrevKey
+        $ M.adjust (\(nextPrevKey, nextValue, nextNextKey) ->
+            (if iterPrevKey == iter then iterNextKey else iterPrevKey, nextValue, nextNextKey)
+          ) iterNextKey
+        $ M.delete iter container
+      }
+
+  fromList [] = empty
+  fromList as = LinkedList
+    { newKey = len
+    , firstKey = 0
+    , lastKey = lastKey
+    , container = M.fromList $ zip [0..] $ zip3 (0 : [0..]) as ([1 .. lastKey] ++ [lastKey])
+    }
+    where
+      lastKey = len - 1
+      len = fromIntegral $ length as
+
+  toList (LinkedList {..})
+    | M.null container = []
+    | otherwise = go firstKey where
+      go key = case M.lookup key container of
+        Just (_, value, nextKey) ->
+          value : (if nextKey == key then [] else go nextKey)
+        _ ->
+          []
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,112 @@
+{-# LANGUAGE ScopedTypeVariables, FlexibleContexts #-}
+
+import Control.Monad
+import System.Exit (exitFailure, exitSuccess)
+import qualified Data.IterLinkedList as L
+
+test :: forall a. L.IterLinkedList a => [L.LinkedList a Int]
+test =
+  let
+    list1 :: L.LinkedList a Int
+    list1 = go 1 (L.firstIter L.empty) L.empty where
+      go 21 _ list = list
+      go value iter list = go (value + 1) iter (L.insertBefore iter value list)
+
+    list2 :: L.LinkedList a Int
+    list2 = go 1 (L.firstIter L.empty) L.empty where
+      go 21 _ list = list
+      go value iter list = go (value + 1) iter (L.insertAfter iter value list)
+
+    list3 :: L.LinkedList a Int
+    list3 = go 1 (L.firstIter L.empty) L.empty where
+      go 21 _ list = list
+      go value iter list =
+        let
+          list' = L.insertBefore iter value list
+          iter' = L.prev list' iter
+        in
+          go (value + 1) iter' list'
+
+    list4 :: L.LinkedList a Int
+    list4 = go 1 (L.firstIter L.empty) L.empty where
+      go 21 _ list = list
+      go value iter list =
+        let
+          list' = L.insertAfter iter value list
+          iter' = L.next list' iter
+        in
+          go (value + 1) iter' list'
+
+    list5 :: L.LinkedList a Int
+    list5 = go 1 L.empty where
+      go 21 list = list
+      go value list = go (value + 1) (L.insertBefore (L.firstIter list) value list)
+
+    list6 :: L.LinkedList a Int
+    list6 = go 1 L.empty where
+      go 21 list = list
+      go value list = go (value + 1) (L.insertAfter (L.lastIter list) value list)
+
+    list7 :: L.LinkedList a Int
+    list7 = go 1 (L.firstIter list5) list5 where
+      go 21 _ list = list
+      go step iter list = go (step + 2) (L.next list' iter) list'
+        where
+          list' = L.delete (L.next list iter) list
+
+    list8 :: L.LinkedList a Int
+    list8 = go 1 (L.lastIter list6) list6 where
+      go 21 _ list = list
+      go step iter list = go (step + 2) (L.prev list' iter) list'
+        where
+          list' = L.delete (L.prev list iter) list
+  in
+    [list1, list2, list3, list4, list5, list6, list7, list8]
+
+answers :: Num a => [[a]]
+answers =
+  [ [2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,1]
+  , [1,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2]
+  , [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+  , [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+  , [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+  , [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+  , [20,18,16,14,12,10,8,6,4,2]
+  , [2,4,6,8,10,12,14,16,18,20]
+  ]
+
+getForward :: forall a. (Eq a, L.IterLinkedList a) => L.LinkedList a Int -> [Int]
+getForward list = go (L.firstIter list) where
+  go iter = L.get' iter list : next where
+    iter' = L.next list iter
+    next
+      | iter' == iter = []
+      | otherwise = go iter'
+
+getBackward :: forall a. (Eq a, L.IterLinkedList a) => L.LinkedList a Int -> [Int]
+getBackward list = go (L.lastIter list) where
+  go iter = L.get' iter list : prev where
+    iter' = L.prev list iter
+    prev
+      | iter' == iter = []
+      | otherwise = go iter'
+
+testSet :: forall a. (Show (L.LinkedList a Int), Eq a, L.IterLinkedList a) => [L.LinkedList a Int] -> IO ()
+testSet ls = do
+  when (map L.toList ls /= answers) $ do
+    putStrLn $ show ls ++ " (Int) against " ++ show answers
+    exitFailure
+
+  when (map getForward ls /= answers) $ do
+    putStrLn $ show ls ++ " (Int forward) against " ++ show answers
+    exitFailure
+
+  when (map getBackward ls /= map reverse answers) $ do
+    putStrLn $ show ls ++ " (Int backward) against " ++ show (map reverse answers)
+    exitFailure
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  testSet (test :: [L.LinkedList Int Int])
+  testSet (test :: [L.LinkedList Integer Int])
+  exitSuccess
